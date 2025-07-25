28f9966036bf6138a440e43d4c733819f65e0537
MAWADA SHAWIR <mawadashawir@gmail.com>
2025-07-25 18:06:21 +0400

    implement navbar

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>New Companion</div>;
+};
+
+export default page;
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const page = () => {
+  return <div>Sign In</div>;
+};
+
+export default page;
